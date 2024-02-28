23d3dc716aa3a6034655ab606dcd788c667f0bb7
iran <iransantana2624@gmail.com>
2024-02-28 08:48:36 -0300

    Update programa.js

--- [POO-Manhã][Aula 2][Alexsander]/programa.js
+++ [POO-Manhã][Aula 2][Alexsander]/programa.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+let frutas = [];
+frutas.push('kiwi')
+frutas.push('banana')
+frutas.push('uva');
+frutas.push('pitaya')
+
+console.log(frutas);
+
+frutas[3] = 'abacaxi';
+
+console.log(frutas)

20fb1892e347fc0b1daf5d7159e234479b2ff45d
iran <iransantana2624@gmail.com>
2024-07-02 08:39:38 -0300

    poo

---
+++ [POO - Manhã][Aula 17][Revisão][Iran]/programa.js
@@ -0,0 +1,57 @@
+let aspire5 = {
+    marca: ' Acer',
+    modelo: "Aspire 5 ",
+    ram: '8GB RAM',
+    processador: 'Intel Core i5 12º Geração',
+    armazenamento: '256GB SSD',
+    tamanhoDaTela: '15.6" LED Full HD',
+    valor: 'R$ 2.429'
+}
+
+let ideapad3 = {
+    marca: 'Lenovo',
+    modelo: "IdeaPad 3",
+    ram: '8GB RAM',
+    processador: 'R7-5700U',
+    armazenamento: '512GB SSD',
+    tamanhoDaTela: '15,6 Polegadas',
+    valor: 3039
+}
+
+let g15 = {
+    marca: 'Dell',
+    modelo: "G15",
+    ram: '16GB DDR5',
+    processador: 'Intel® Core™ i7-13650HX',
+    armazenamento: '512 GB SSD',
+    tamanhoDaTela: 'Tela 15.6” Full HD',
+    valor: 5921
+}
+
+let nitro5 = {
+    marca: 'Acer ',
+    modelo: "Nitro 5",
+    ram: ' 8 GB',
+    processador: 'Intel Core i5 2450H',
+    armazenamento: '512 GB SSD',
+    tamanhoDaTela: '15.6 Polegadas',
+    valor: 4420
+}
+
+let tuf= {
+    marca: 'Asus',
+    modelo: "TUF Gaming F15",
+    ram: '8GB RAM',
+    processador: 'Intel Core I5 12500H',
+    armazenamento: '512GB SSD',
+    tamanhoDaTela: 'Tela de 15,6',
+    valor: 5585
+}
+
+let listaDeNotebook = [];
+
+listaDeNotebook.push(aspire5);
+listaDeNotebook.push(ideapad3);
+listaDeNotebook.push(g15);
+listaDeNotebook.push(nitro5);
+listaDeNotebook.push(tuf);

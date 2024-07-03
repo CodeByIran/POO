d282857137f3b67713b51e99e68c7d8adac4228d
iran <iransantana2624@gmail.com>
2024-07-03 08:42:52 -0300

    poo

---
+++ [POO - Manhã][Aula 18][Classes][Iran]/classes.js
@@ -0,0 +1,22 @@
+//Nome de classe sempre deve começar com letra maiúscula
+class Aluno{ 
+    //O constructor cria o objeto
+    constructor(nome, matricula, cidade, idade, curso){ 
+        //Nos parênteses definimos as entradas
+        
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cidade = cidade;
+        this.idade = idade;
+        this.curso = curso;
+    }
+}
+
+class cafe{
+    
+    constructor(po, agua, acucar){
+        this.po = po;
+        this.agua = agua;
+        this.acucar = acucar;
+    }
+}
---
+++ [POO - Manhã][Aula 18][Classes][Iran]/programa.js
@@ -0,0 +1,6 @@
+let alunoTal = new Aluno('Adriely', '2023talecoisa', 'Rio largo');
+//let bala = mew Body(40, 'blue', )
+
+console.log(alunoTal.nome);
+console.log(alunoTal.matricula);
+console.log(alunoTal.cidade);

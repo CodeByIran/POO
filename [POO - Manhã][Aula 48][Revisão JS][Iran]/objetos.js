f743eb927fda1bf8b6f0090afeb2f4b6e666f92a
Iran <iransantana2624@gmail.com>
2024-10-09 08:54:23 -0300

    js

---
+++ [POO - Manhã][Aula 48][Revisão JS][Iran]/objetos.js
@@ -0,0 +1,33 @@
+let lancheA = {
+    nome: "X-Burger",
+    tipo: "Hamburguer",
+    foto: "xburguer.jpeg",
+    preco: 18.90
+  };
+
+  let lancheB  = {
+    nome: "X-Calabresa",
+    tipo: "Hamburguer",
+    foto: "xcalabresa.jpeg",
+    preco: 13.00
+  };
+
+  let lancheC = {
+    nome: "X-Alcatra",
+    tipo: "Hamburguer",
+    foto: "xalcatra.jpeg",
+    preco: 14.90
+  };
+
+  let lancheD = {
+    nome: "X-Picanha",
+    tipo: "Hamburguer",
+    foto: "xpicanha.jpeg",
+    preco: 29.90
+  };
+
+  let cardapio = [];
+  cardapio.push(lancheA);
+  cardapio.push(lancheB);
+  cardapio.push(lancheC);
+  cardapio.push(lancheD);

0724b3451b6195081fdfb3509e177668ce3f5b97
Iran <iransantana2624@gmail.com>
2025-01-16 08:57:42 -0300

    react

--- React/netflix2/src/components/MenuLateral.js
+++ React/netflix2/src/components/MenuLateral.js
@@ -1,9 +1,24 @@
 import './MenuLateral.css';
+import { FaHome } from 'react-icons/fa';
+import { FaCreditCard } from 'react-icons/fa';
+import { FaCheckCircle } from 'react-icons/fa';
+import { FaLaptop } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
 
 function MenuLateral() {
     return (
         <div className='menu-lateral'>
             {/* Add your menu content here */}
+            <a className='link selecionado'> Visão Geral </a>
+            <FaHome /> 
+            <a className='link'> Assinatura </a>
+            <FaCreditCard />
+            <a className='link'> Segurança </a>
+            <FaCheckCircle /> 
+            <a className='link'> Aparelhos   </a>
+            <FaLaptop /> 
+            <a className='link'> Perfis</a>
+            <FaUser /> 
         </div>
     );
 }

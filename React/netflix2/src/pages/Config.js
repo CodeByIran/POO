c8f5792815d4345947a88cb4fad995b7713e31ed
Iran <iransantana2624@gmail.com>
2025-01-16 10:42:55 -0300

    React

---
+++ React/netflix2/src/pages/Config.js
@@ -0,0 +1,16 @@
+/* Criem o componente Config */
+/* Ele será a nossa página de Configuração do Netflix */
+//Por enquanto, essa página só terá o menu lateral
+import React from 'react';
+import './Config.css'
+import MenuLateral from '../components/MenuLateral';
+
+function Config() {
+  return (
+    <div>
+      <MenuLateral />
+    </div>
+  );
+}
+
+export default Config;
---
+++ React/netflix2/src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+import './Home.css'
+import NavBar from '../components/NavBar';
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
